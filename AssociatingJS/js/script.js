902a81b42d0fabf160a84d99078bea0132779f5b
KellieDee <kdpicke@gmail.com>
2015-02-12 14:55:21 -0600

    Activity 4
    Screencast demonstration of activity 4

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,75 +1,12 @@
-
 /*
-var a = 2;
-var b;
-b = a + 3;
-console.log(a);
-
-
-var yearBorn = 1989
-var age = 2015 - yearBorn - 1;
-console.log(age);
-
-
-// Area of a triangle = 1/2bh
-var base = 5;
-var height = 4;
-var area = base * height/2;
-console.log(area);
-
-
-var remainder = 211 % 2;
-console.log(remainder);
-
-
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-// console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width * 2
-console.log(perimeter);
-
-
-var orangeBins =[234, 567, 883];
-
-var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
-console.log(total);
-
-
-var firstName = "Kellie";
-var lastName = "Pickering";
-var fullName = firstName + " " + lastName;
-console.log(fullName);
-
-
-var a =6;
-var b =7;
-
-var result = a + b;
-console.log(result);
-
-
-var stringVar = "6";
-var result = 7 + Number(stringVar); //casting
-console.log(result);
-
-
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode) +")" + String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
-
-
-var a = 3;
-// a ++ ; // a = a + 1  // a += 1
-a--; // a = a - 1 // a -= 1
-console.log(a);
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
 */
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width * height;
+// console.log(area);
+
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result);
