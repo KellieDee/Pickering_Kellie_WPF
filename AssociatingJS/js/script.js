db02bd9118df35ea64a0a5ec09497971ef72354f
KellieDee <kdpicke@gmail.com>
2015-02-12 14:04:13 -0600

    Activities 1 + 2
    Screencast activities from activities 1 + 2 (commented out as I go)

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * Created by KellieDee on 2/12/15.
+ */
+
+// alert("Hello World!");
+
+// var yearBorn = 1989
+
+// var age; //declaration
+// age = 25; //definition
+
+// var yearOfBirth = 1989; //declaring and defining
+
+//declare before defining
+
+// var isStudent = true;
+// var firstName = "Kellie";
+// var phrase = 'I don\'t know \n Ask the teacher';
+
+// var muppetNames = ["Kermit", "Gonzo", "Rizzo"]; //0,1,2
+// muppetNames[1] = "Miss Piggy";
+// console.log(muppetNames[1]);
+
+/*
+console.log(yearBorn);
+alert(yearBorn);
+    */
