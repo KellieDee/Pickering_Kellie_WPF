1b2341538f1282e09ef41bce15d05be55d8465a7
KellieDee <kdpicke@gmail.com>
2015-02-12 15:29:45 -0600

    Expressions Worksheet Problem 1
    Wrote an expression to find Sparky’s age in dog years.

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,14 @@
+// Kellie Pickering
+// 02/12/15
+// Expression Worksheet
+
+
+// Finding Sparky's Age in Dog Years
+var humanYears = 5;
+const dogYears = 7;
+
+// Multiply age in human years by 7 to find age in dog years.
+var age = humanYears * dogYears;
+
+// Print out the output age
+console.log("Sparky is " + humanYears + " human years old which is " + age + " in dog years.");
