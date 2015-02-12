1deec5aecf9990f89b7a208221bcaa10fa48813d
KellieDee <kdpicke@gmail.com>
2015-02-12 14:42:47 -0600

    Activity 3
    Screencast demonstrations from Activity 3

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,27 +1,75 @@
-/**
- * Created by KellieDee on 2/12/15.
- */
-
-// alert("Hello World!");
-
-// var yearBorn = 1989
-
-// var age; //declaration
-// age = 25; //definition
-
-// var yearOfBirth = 1989; //declaring and defining
-
-//declare before defining
-
-// var isStudent = true;
-// var firstName = "Kellie";
-// var phrase = 'I don\'t know \n Ask the teacher';
-
-// var muppetNames = ["Kermit", "Gonzo", "Rizzo"]; //0,1,2
-// muppetNames[1] = "Miss Piggy";
-// console.log(muppetNames[1]);
 
 /*
-console.log(yearBorn);
-alert(yearBorn);
-    */
+var a = 2;
+var b;
+b = a + 3;
+console.log(a);
+
+
+var yearBorn = 1989
+var age = 2015 - yearBorn - 1;
+console.log(age);
+
+
+// Area of a triangle = 1/2bh
+var base = 5;
+var height = 4;
+var area = base * height/2;
+console.log(area);
+
+
+var remainder = 211 % 2;
+console.log(remainder);
+
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+// console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2
+console.log(perimeter);
+
+
+var orangeBins =[234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
+
+var firstName = "Kellie";
+var lastName = "Pickering";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+var a =6;
+var b =7;
+
+var result = a + b;
+console.log(result);
+
+
+var stringVar = "6";
+var result = 7 + Number(stringVar); //casting
+console.log(result);
+
+
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+
+var phoneNo = "(" + String(areaCode) +")" + String(firstPart) + "-" + String(secPart);
+console.log(phoneNo);
+
+
+var a = 3;
+// a ++ ; // a = a + 1  // a += 1
+a--; // a = a - 1 // a -= 1
+console.log(a);
+*/
