f590f504f59bc187824093460f22365cac30fe7c
KellieDee <kdpicke@gmail.com>
2015-02-12 17:53:58 -0600

    User Prompts
    Determined variables based on five user prompts.

--- Pickering_Kellie_Expressions_Assignment/js/script.js
+++ Pickering_Kellie_Expressions_Assignment/js/script.js
@@ -7,6 +7,13 @@
 // alert("Javascript works!");
 
 // Determining total income over the weekend and finding the average amount of money earned per day
-var = wage;
-var = hoursWorked;
-var = tips ["Friday", "Saturday", "Sunday"];
+
+// We ask the user for some basic information to determine variables.
+var wage = prompt("We will calculate your weekend income. \nWhat is your hourly wage?");
+var hoursWorked = prompt("How many hours did you work Friday through Sunday?");
+var friday = prompt("What did you earn in tips on Friday?");
+var saturday = prompt("What did you earn in tips on Saturday?");
+var sunday = prompt("What did you earn in tips on Sunday?");
+
+//var = tips ["Friday", "Saturday", "Sunday"];
+// console.log("You worked X hours this weekend making X in wage and X in tips for a total of X all together. This is an average of X per day.");
