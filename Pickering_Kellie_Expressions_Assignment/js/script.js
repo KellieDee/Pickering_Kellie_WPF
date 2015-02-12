24a7d02a9d7c38c718edea59437e739a466a0fd3
KellieDee <kdpicke@gmail.com>
2015-02-12 17:41:48 -0600

    Assignment skeleton
    Chose a theme for the assignment and set up variables.

---
+++ Pickering_Kellie_Expressions_Assignment/js/script.js
@@ -0,0 +1,12 @@
+/*
+    Kellie Pickering
+    05/12/15
+    Expressions
+*/
+
+// alert("Javascript works!");
+
+// Determining total income over the weekend and finding the average amount of money earned per day
+var = wage;
+var = hoursWorked;
+var = tips ["Friday", "Saturday", "Sunday"];
