154197155ec1be029b1c6798033d3f961f0cdfff
KellieDee <kdpicke@gmail.com>
2015-02-12 15:44:01 -0600

    Expressions Worksheet Problem 2
    Wrote an expression that determined how many slices of pizza each
    person consumed at a party.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,10 +5,27 @@
 
 // Finding Sparky's Age in Dog Years
 var humanYears = 5;
-const dogYears = 7;
+const dogRate = 7;
 
-// Multiply age in human years by 7 to find age in dog years.
-var age = humanYears * dogYears;
+// Multiply age in human years by dog rate of growth to find age in dog years.
+var age = humanYears * dogRate;
 
 // Print out the output age
 console.log("Sparky is " + humanYears + " human years old which is " + age + " in dog years.");
+
+
+
+// Slice of Pie part I
+var slicePerPizza = 8;
+var peopleAtParty = 50;
+var pizzasOrdered = 15;
+
+// Multiply the amount of pizzas ordered by the amount of slices per pizza to find total slices.
+// Divide this product by the amount of people at the party to determine how many pieces each person gets.
+var slicesPerPerson = (pizzasOrdered * slicePerPizza) / peopleAtParty;
+
+// Print out the output slicesPerPerson
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+
+
+
