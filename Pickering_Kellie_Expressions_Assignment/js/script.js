89f7c6aa44a5f81dea11bc6022cfaef2c78698de
KellieDee <kdpicke@gmail.com>
2015-02-12 18:36:13 -0600

    Casting and Completion
    Casted array elements and completed the expression.

--- Pickering_Kellie_Expressions_Assignment/js/script.js
+++ Pickering_Kellie_Expressions_Assignment/js/script.js
@@ -6,14 +6,27 @@
 
 // alert("Javascript works!");
 
-// Determining total income over the weekend and finding the average amount of money earned per day
+// Determining average daily income over the weekend.
 
-// We ask the user for some basic information to determine variables.
+// Ask the user for some basic information to determine variables.
+const daysWorked = 3
 var wage = prompt("We will calculate your weekend income. \nWhat is your hourly wage?");
 var hoursWorked = prompt("How many hours did you work Friday through Sunday?");
 var friday = prompt("What did you earn in tips on Friday?");
 var saturday = prompt("What did you earn in tips on Saturday?");
 var sunday = prompt("What did you earn in tips on Sunday?");
 
-//var = tips ["Friday", "Saturday", "Sunday"];
-// console.log("You worked X hours this weekend making X in wage and X in tips for a total of X all together. This is an average of X per day.");
+// Set up an array with daily tip amounts based on user prompts.
+var tips = [friday, saturday, sunday];
+
+// Add up all daily tips to find the total amount.
+var incomeTips = Number(tips[0]) + Number(tips[1]) + Number(tips[2]);
+
+// Determine the amount of income earned before tips by multiplying hourly wage by the amount of hours worked.
+var incomeWage = wage * hoursWorked;
+
+// Determine the average earned per day by adding income from wages and tips, then dividing by the amount of days worked.
+var average = (incomeWage + incomeTips) / daysWorked;
+
+// Print the output of average money earned per day.
+console.log("You worked " + hoursWorked + " hours this weekend and made " + incomeWage + " in wages and " + incomeTips + " in tips, for an average of " + average + " per day.");
