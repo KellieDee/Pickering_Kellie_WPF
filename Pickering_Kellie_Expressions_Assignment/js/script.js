9a60ecce90e49c4fdfd27846c0a8fb0216ae3d2f
KellieDee <kdpicke@gmail.com>
2015-02-12 18:57:18 -0600

    Test Run
    Input prompts to test the expression.

--- Pickering_Kellie_Expressions_Assignment/js/script.js
+++ Pickering_Kellie_Expressions_Assignment/js/script.js
@@ -6,7 +6,9 @@
 
 // alert("Javascript works!");
 
+
 // Determining average daily income over the weekend.
+
 
 // Ask the user for some basic information to determine variables.
 const daysWorked = 3
@@ -28,5 +30,21 @@ var incomeWage = wage * hoursWorked;
 // Determine the average earned per day by adding income from wages and tips, then dividing by the amount of days worked.
 var average = (incomeWage + incomeTips) / daysWorked;
 
+// Round the average to the nearest whole number to eliminate unrealistic change amounts.
+var rndAvg = Math.round(average);
+
 // Print the output of average money earned per day.
-console.log("You worked " + hoursWorked + " hours this weekend and made " + incomeWage + " in wages and " + incomeTips + " in tips, for an average of " + average + " per day.");
+console.log("You worked " + hoursWorked + " hours this weekend and made " + incomeWage + " in wages and " + incomeTips + " in tips, for an average of " + rndAvg + " per day.");
+
+
+/*
+    Tested the expression by inputting the following:
+
+    Wage: 4.30
+    Hours Worked: 20
+    Tips on Friday: 5
+    Tips on Saturday: 5
+    Tips on Sunday: 5
+
+    The console concluded I had worked 20 hours for a total of $86 in wages, $15 in tips, and an average of $34 per day.
+ */
