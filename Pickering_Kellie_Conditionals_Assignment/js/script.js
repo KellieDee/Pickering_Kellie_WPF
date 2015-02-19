ad0e4cd199ff4b87288158f88b293c80b4b7a7d1
KellieDee <kdpicke@gmail.com>
2015-02-19 16:52:17 -0600

    Conditional Variables
    Set up the variables I will be using to calculate the user’s birthday.

--- Pickering_Kellie_Conditionals_Assignment/js/script.js
+++ Pickering_Kellie_Conditionals_Assignment/js/script.js
@@ -3,3 +3,10 @@
     05/19/15
     Conditionals Assignment
  */
+
+// BIRTHDAY CALCULATOR
+var currentMonth = 2; // January = 1, February = 2, March = 3, etc.
+var daysPerMonth = 30; // average days per month
+var today;
+var monthsAway;
+var daysAway;
