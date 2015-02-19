b5ac63ef04128db0d4ee063d25b0d1cc9c2f60cc
KellieDee <kdpicke@gmail.com>
2015-02-19 17:04:36 -0600

    Birthday prompt
    Asked the user if their birthday was today, and if so, wished them a
    happy birthday.

--- Pickering_Kellie_Conditionals_Assignment/js/script.js
+++ Pickering_Kellie_Conditionals_Assignment/js/script.js
@@ -10,3 +10,15 @@ var daysPerMonth = 30; // average days per month
 var today;
 var monthsAway;
 var daysAway;
+
+//Ask the user if their birthday is today.
+today = prompt("Is today your birthday? \nType 'yes' or 'no'.");
+
+//If today is their birthday, wish them a happy birthday.
+if (today === "yes"){
+    console.log("Happy birthday!");
+}
+
+// prompt("Is your birthday later in the month?");
+
+// prompt("How many months away is your birthday?");
