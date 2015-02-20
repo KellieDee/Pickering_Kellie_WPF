296f94940e2cd16e04adc367b053ebe90d8fd533
KellieDee <kdpicke@gmail.com>
2015-02-19 18:20:33 -0600

    Conditionals Final
    Added a ternary operated and tested the code. Results are left in
    comments.

--- Pickering_Kellie_Conditionals_Assignment/js/script.js
+++ Pickering_Kellie_Conditionals_Assignment/js/script.js
@@ -10,7 +10,6 @@ var daysPerMonth = 30; // average days per month
 var today;
 var thisMonth;
 var birthdayMonth;
-var monthsAway;
 var daysAway;
 
 //Ask the user if their birthday is today.
@@ -22,22 +21,25 @@ thisMonth = prompt("Is your birthday later this month? \nType 'yes' or 'no'");
 //Ask the user what month their birthday is in.
 birthdayMonth = prompt("What month is your birthday in? \nType '1' for January, '2' for February, etc.");
 
-//Calculate the amount of months until next birthday
-monthsAway = birthdayMonth - currentMonth;
-
 //Calculate the amount of days until next birthday
-daysAway = Math.round(monthsAway * daysPerMonth);
+daysAway = Math.round((birthdayMonth - currentMonth) * daysPerMonth);
 
 //If today is their birthday, wish them a happy birthday.
-if(today === "yes"){
-    console.log("Happy birthday!");
-}
+today === "yes" ? console.log("Happy birthday!") : console.log("Happy \"Unbirthday\" to you! \:\)");
+
 
 //If their birthday isn't today but is later in the month, wish them a happy early birthday.
 if(today === "no" && thisMonth === "yes"){
-    console.log("Happy early birthday!");
-}else if(thisMonth === "no" && birthdayMonth < 8 && birthdayMonth > 2){
-    console.log("Not much longer! Only " + daysAway + " days left!");
+    console.log("Your real birthday is right around the corner!");
+}else if(thisMonth === "no" && birthdayMonth <= 8 && birthdayMonth > 2){
+    console.log("Not much longer until your real birthday! Only " + daysAway + " days left!");
 }else{
-    console.log("You've got a ways to go until your next birthday!");
+    console.log("You've got a ways to go until your next real birthday!");
 }
+
+/*
+    Tested code with "yes" "no" "2"     Result: Happy birthday!     [...] ways to go
+    Tested code with "no" "yes" "2"     Result: Happy unbirthday!   [...] right around the corner
+    Tested code with "no" "no" "8"      Result: Happy unbirthday!   [...] 180 days left
+    Tested code with "no" "no" "1"      Result: Happy unbirthday!   [...] ways to go
+ */
