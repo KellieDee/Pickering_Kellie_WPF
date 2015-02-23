861d12c9e47989ad5e036817ea687c936855724e
KellieDee <kdpicke@gmail.com>
2015-02-23 15:17:39 -0600

    Florida Lottery Numbers
    Set up the basic structure of a function that returns six random
    numbers for the Florida lottery. Still need to validate user prompt and
    disable duplicate numbers in console.

--- Pickering_Kellie_Functions_Assignment/js/script.js
+++ Pickering_Kellie_Functions_Assignment/js/script.js
@@ -4,10 +4,23 @@
     Functions Assignment
  */
 
-// Ask the user which lottery they want to play.
+// Ask the user which lottery they want to play
 var userPrompt = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
 
+// Assign the function to variable values
+var fn1 = floridaRNG();
+var fn2 = floridaRNG();
+var fn3 = floridaRNG();
+var fn4 = floridaRNG();
+var fn5 = floridaRNG();
+var fn6 = floridaRNG();
 
-function randomNumber(n){
-    return n;
+// Function that generates a random number 1 - 53 for the Florida Lottery
+function floridaRNG(){
+    return (Math.floor(Math.random() * 53) + 1);
+}
+
+// Print the Florida Lottery numbers to the console
+if(userPrompt === "Florida") {
+    console.log("Your numbers are " + fn1 + " " + fn2 + " " + fn3 + " " + fn4 + " " + fn5 + " " + fn6);
 }
