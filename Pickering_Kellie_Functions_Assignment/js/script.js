2ab31991884db9c50875ceb87ad55bad6b3558fc
KellieDee <kdpicke@gmail.com>
2015-02-23 14:35:14 -0600

    Prompt and Function
    Created a user prompt asking which lottery to play. Started a function
    to assist with random number generation.

--- Pickering_Kellie_Functions_Assignment/js/script.js
+++ Pickering_Kellie_Functions_Assignment/js/script.js
@@ -4,4 +4,10 @@
     Functions Assignment
  */
 
-alert("Javascript works!");
+// Ask the user which lottery they want to play.
+var userPrompt = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
+
+
+function randomNumber(n){
+    return n;
+}
