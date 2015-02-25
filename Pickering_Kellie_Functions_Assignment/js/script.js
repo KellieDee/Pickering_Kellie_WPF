e5951a3bc2a6278a1ca76719922118053886cf90
KellieDee <kdpicke@gmail.com>
2015-02-25 15:05:49 -0600

    Reorganization
    Moved code into sections: Variables, Functions, and Main Code.
    Removed individual Florida number variables in order to start an array.

--- Pickering_Kellie_Functions_Assignment/js/script.js
+++ Pickering_Kellie_Functions_Assignment/js/script.js
@@ -4,30 +4,36 @@
     Functions Assignment
  */
 
+// VARIABLES
+
+var lottery; // Which lottery the user wants to play
+var floridaNumbers; // Six numbers between 1 and 53
+var powerballNumbers; // Five numbers between 1 and 59
+var powerball; // One Powerball number between 1 and 35.
+
+// FUNCTIONS
+
 // Ask the user which lottery they want to play
-var userPrompt = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
+var lottery = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
 
 //Validate the user's response
-while(userPrompt !== "Florida" && userPrompt !== "Powerball"){
+while(lottery !== "Florida" && lottery !== "Powerball"){
     // If the user's response is not Florida or Powerball, the script will prompt again
     prompt("You didn't choose a lottery to play! \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
     break;
 }
 
-// Assign the function to variable values
-var fn1 = floridaRNG();
-var fn2 = floridaRNG();
-var fn3 = floridaRNG();
-var fn4 = floridaRNG();
-var fn5 = floridaRNG();
-var fn6 = floridaRNG();
-
 // Function that generates a random number 1 - 53 for the Florida Lottery
-function floridaRNG(){
+function floridaRNG() {
     return (Math.floor(Math.random() * 53) + 1);
 }
 
-// Print the Florida Lottery numbers to the console
-if(userPrompt === "Florida") {
-    console.log("Your numbers are " + fn1 + " " + fn2 + " " + fn3 + " " + fn4 + " " + fn5 + " " + fn6);
+// MAIN CODE
+
+if(lottery === "Florida") {
+    // Print the Florida Lottery numbers to the console
+    console.log("Your Florida lottery numbers are " + floridaNumbers + ".");
+}else{
+    // Print the Powerball Lottery numbers to the console
+    console.log("Your Powerball lottery numbers are " + powerballNumbers + " and your Powerball is " + powerball + ".");
 }
