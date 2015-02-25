a7a2ac0848ec0e566395e95c3de8bb0caeb28efb
KellieDee <kdpicke@gmail.com>
2015-02-25 16:29:44 -0600

    Powerball Function
    Duplicated Florida function and edited for Powerball array. Still need
    to set up function for the Powerball itself and ensure numbers in array
    are unique.

--- Pickering_Kellie_Functions_Assignment/js/script.js
+++ Pickering_Kellie_Functions_Assignment/js/script.js
@@ -6,7 +6,8 @@
 
 // VARIABLES
 
-var lottery; // Which lottery the user wants to play
+// Prompt that asks the user which lottery they want to play
+var lottery = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
 var floridaNumbers; // Six numbers between 1 and 53
 var powerballNumbers; // Five numbers between 1 and 59
 var powerball; // One Powerball number between 1 and 35.
@@ -16,8 +17,8 @@ var powerball; // One Powerball number between 1 and 35.
 // Function that validates the user's response
 function lotteryValidation(lotteryChoice){
     // If the user's response is not "Florida" or "Powerball", the script will prompt again.
-    while(lotteryChoice !== "Florida" && lottery !== "Powerball"){
-        lottery = prompt("You didn't choose a lottery to play! \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
+    while(lotteryChoice !== "Florida" && lotteryChoice !== "Powerball"){
+        lotteryChoice = prompt("You didn't choose a lottery to play! \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
     }
 }
 
@@ -36,22 +37,38 @@ function floridaRNG(min, max){
     return lotteryArray;
 }
 
+// Function that determines five random Powerball lottery numbers
+function powerballRNG(min, max) {
+    // Create an array that stores the lottery numbers
+    var lotteryArray = [];
+    // Set the array index to 0 and add one random number until there are five random numbers ([0] - [4])
+    for (var i = 0; i < 5; i++) {
+        // Math object that generates a random number in a specified range of numbers, then rounds down to the nearest whole number
+        var randomNumber = (Math.floor(Math.random() * max) + min);
+        // Set the elements in the array equal to a random number
+        lotteryArray[i] = randomNumber;
+    }
+    // When the function is called, the results of the array will be shown to the user
+    return lotteryArray;
+}
+
 
 // MAIN CODE
 
-// Ask the user which lottery they want to play
-lottery = prompt("Which lottery would you like to play? \n\nType \"Florida\" for the Florida Lottery. \nType \"Powerball\" for the Powerball Lottery.");
-
 // Validate the user's response
-lotteryValidation(lottery);
+lottery = lotteryValidation(lottery);
 
 // Set the Florida Lottery function to a variable and input the minimum and maximum numbers
 floridaNumbers = floridaRNG(1, 53);
+
+// Set the Powerball Lottery function to a variable and input the minimum and maximum numbers
+powerballNumbers = powerballRNG(1, 59);
+
 
 if(lottery === "Florida") {
     // If the user answers "Florida", print the Florida Lottery numbers to the console
     console.log("Your Florida lottery numbers are " + floridaNumbers + ".");
 }else{
     // If the user does not answer "Florida", print the Powerball Lottery numbers to the console
-    console.log("Your Powerball lottery numbers are " + powerballNumbers + " and your Powerball is " + powerball + ".");
+    console.log("Your Powerball lottery numbers are " + powerballNumbers + " and the Powerball is " + powerball + ".");
 }
